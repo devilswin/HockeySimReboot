f9cdef1d21853e3684c7e961c5392a9ebed4f16e
devilswin <orlanbob@yahoo.com>
2014-09-12 10:40:46 -0400

    Reboot Of the Hockey Sim Game
    dev Version 0.0.0

---
+++ HockeySimReboot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HockeySimReboot
+//
+//  Created by GO on 9/12/14.
+//  Copyright (c) 2014 GO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
